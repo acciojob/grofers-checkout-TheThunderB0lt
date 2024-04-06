9d726ec2e151b02a7eb945501952ac6d7e1cedde
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-04-06 22:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let table = document.querySelector("table")
+let itemPrice = document.querySelectorAll(".price")
+
 const getSum = () => {
-//Add your code here
-  
+	let newRow = document.createElement("tr")
+	newRow.id = "ans"
+	let newCell = document.createElement("td")
+	let totalPrice = 0
+	for(let t of itemPrice){
+		totalPrice += +t.innerText
+	} 
+	newCell.innerText = `${totalPrice}`
+	newRow.append(newCell)
+	table.append(newRow)
 };
 
 getSumBtn.addEventListener("click", getSum);
-
